5a9ad4cfc0d3272f38bc5af8c40d1abbe5f09568
David Burke <david.burke@fundingcircle.com>
2017-07-24 07:45:20 -0700

    Added Photo class

---
+++ Photorama/Photo.swift
@@ -0,0 +1,25 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by David Burke on 7/24/17.
+//  Copyright © 2017 amberfire. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.remoteURL = remoteURL
+        self.photoID = photoID
+        self.dateTaken = dateTaken
+    }
+    
+}
