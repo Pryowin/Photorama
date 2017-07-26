fc6efb81f0ba3f284e358c7323ab260bb298ebb0
David Burke <david.burke@fundingcircle.com>
2017-07-26 13:29:15 -0700

    Display first image

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -23,12 +23,26 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print("Found  \(photos.count) photos")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
             case let .failure(error):
                 print("Error fetching photos \(error)")
                 
             }
         }
     }
-
+    
+    func updateImageView (for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    }
 
 }
